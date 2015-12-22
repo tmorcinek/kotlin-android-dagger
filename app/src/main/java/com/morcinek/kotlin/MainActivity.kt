1b253d898bc07023431fe8ffe9b78b1a55f2a860
Tomasz Morcinek <tomasz@morcinek.co.uk>
2015-12-22 12:11:56 +0700

    Converting project to Kotlin.

---
+++ app/src/main/java/com/morcinek/kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.morcinek.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.morcinek.kotlin.R.layout.main)
+    }
+}
