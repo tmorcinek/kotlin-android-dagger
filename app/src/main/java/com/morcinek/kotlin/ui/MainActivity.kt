9ac2b926bd0d65229a5b0578b1805ac3c73cc2ae
Tomasz Morcinek <tomasz@morcinek.co.uk>
2015-12-23 09:45:38 +0700

    Usign injected object.

--- app/src/main/java/com/morcinek/kotlin/ui/MainActivity.kt
+++ app/src/main/java/com/morcinek/kotlin/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.morcinek.kotlin.ui
 import android.location.LocationManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.morcinek.kotlin.MainApplication
 import com.morcinek.kotlin.R
 import javax.inject.Inject
 
@@ -17,5 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        (application as MainApplication).component.inject(this)
+
+        (findViewById(R.id.mainText) as TextView).text = locationManager.toString();
     }
 }
